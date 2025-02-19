447a9671873a12a764d29413d5de1d43da037f49
dannightmare <dan.kusai.star.123234@gmail.com>
2025-02-19 13:51:56 +0200

    grains.c 2nd iteration

--- c/grains/grains.c
+++ c/grains/grains.c
@@ -1,11 +1,9 @@
 #include "grains.h"
+#include <stdint.h>
 
 uint64_t square(uint8_t index)
 {
-    if (index > 64 || index == 0) {
-        return 0;
-    }
-    return (uint64_t)1 << (index - 1);
+    return (index > 64 || index == 0) ? 0 : (uint64_t)1 << (index - 1);
 }
 
 uint64_t total(void)

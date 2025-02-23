bdba2d50f110f4ae31c1494dc9a441ecac1101ec
Daniel Kusai <dan.kusai@gmail.com>
2025-02-23 21:33:32 +0200

    queen_attack.c - iteration

--- c/queen-attack/queen_attack.c
+++ c/queen-attack/queen_attack.c
@@ -1,5 +1,6 @@
 #include "queen_attack.h"
 #include <stdbool.h>
+#include <stdlib.h>
 
 bool is_valid_position(position_t queen)
 {
@@ -15,8 +16,7 @@ attack_status_t can_attack(position_t queen_1, position_t queen_2)
     }
     if (queen_1.row == queen_2.row ||
         queen_1.column == queen_2.column ||
-        (queen_1.column - queen_1.row) == (queen_2.column - queen_2.row) ||
-        (queen_1.column + queen_1.row) == (queen_2.column + queen_2.row)) {
+        abs(queen_1.row - queen_2.row) == abs(queen_1.column - queen_2.column)) {
         return CAN_ATTACK;
     }
     return CAN_NOT_ATTACK;

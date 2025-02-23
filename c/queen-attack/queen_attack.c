ae806fd4f22d665afdd5458014bbf745ece757e1
Daniel Kusai <dan.kusai@gmail.com>
2025-02-23 21:31:18 +0200

    queen_attack c - corrected a bug

--- c/queen-attack/queen_attack.c
+++ c/queen-attack/queen_attack.c
@@ -1,23 +1,22 @@
 #include "queen_attack.h"
 #include <stdbool.h>
 
-bool is_invalid_position(position_t queen)
+bool is_valid_position(position_t queen)
 {
-    return queen.row >= 0 && queen.row <= 7 && queen.column >= 0 &&
-           queen.column <= 7;
+    return queen.row <= 7 && queen.column <= 7;
 }
 
 attack_status_t can_attack(position_t queen_1, position_t queen_2)
 {
-    if (is_invalid_position(queen_1) ||
-        is_invalid_position(queen_2) ||
+    if (!is_valid_position(queen_1) ||
+        !is_valid_position(queen_2) ||
         (queen_1.row == queen_2.row && queen_1.column == queen_2.column)) {
         return INVALID_POSITION;
     }
     if (queen_1.row == queen_2.row ||
         queen_1.column == queen_2.column ||
-        queen_1.column - queen_1.row == queen_2.column - queen_2.column ||
-        queen_1.column + queen_1.row == queen_2.column + queen_2.column) {
+        (queen_1.column - queen_1.row) == (queen_2.column - queen_2.row) ||
+        (queen_1.column + queen_1.row) == (queen_2.column + queen_2.row)) {
         return CAN_ATTACK;
     }
     return CAN_NOT_ATTACK;
--- c/queen-attack/queen_attack.h
+++ c/queen-attack/queen_attack.h
@@ -2,6 +2,7 @@
 #define QUEEN_ATTACK_H
 
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef enum { CAN_NOT_ATTACK, CAN_ATTACK, INVALID_POSITION } attack_status_t;
 
@@ -10,6 +11,7 @@ typedef struct {
    uint8_t column;
 } position_t;
 
+bool is_valid_position(position_t queen);
 attack_status_t can_attack(position_t queen_1, position_t queen_2);
 
 #endif
